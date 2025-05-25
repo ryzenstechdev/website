3dddc1866896d09a242d513a16a29f38c87c6621
exystech <ryzenstech@gmail.com>
2025-05-25 20:44:30 +0200

    Gyattload
    yes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/bleach-core' : '',
+  basePath: '/bleach-core',
   images: {
     unoptimized: true,
     domains: ['raw.githubusercontent.com'],
   },
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/bleach-core/' : '',
+  assetPrefix: '/bleach-core/',
+  distDir: 'dist',
 }
 
 module.exports = nextConfig 
