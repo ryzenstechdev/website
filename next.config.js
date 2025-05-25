fbe46ede8681f2f38a8786dce2a22ac08612210e
ryzen's tech! <158189251+ryzenstechdev@users.noreply.github.com>
2025-05-25 20:27:14 +0200

    Lmao forgot dis

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/bleach-core' : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
